3c0dc6b2ea8c2c96a5329f219465a7170eb48835
peter279k <peter279k@gmail.com>
2020-06-03 20:44:17 +0800

    Move vendor file to current file path

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,7 @@
 use DI\ContainerBuilder;
 use Slim\App;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
 
